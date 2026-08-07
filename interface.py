ae1b891a0158a1ce17ac20ffccb98dbee743a6b1
Kauã marcos branco mello <k.brancomello@gmail.com>
2026-08-07 00:29:22 -0300

    criação interface gráfica inicial com Tkinter
    -foi adicionada uma janela para interface do HQ Launcher

---
+++ interface.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+def iniciar_interface():
+
+    janela = tk.Tk()
+
+    janela.title("HQ Launcher")
+
+    janela.geometry("900x600")
+
+    janela.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from biblioteca import carregar_hqs
 from Launcher import abrir_hq
-
+from interface import iniciar_interface
 hqs = carregar_hqs()
 
-if hqs:
-    abrir_hq(hqs[8])
+#if hqs:
+    #abrir_hq(hqs[8])
+
+if __name__ == "__main__":
+    iniciar_interface()
+
+
 
 
 #for hq in hqs:
