085ed64958582716db7bf8d2a5547fa776a20d76
Kauã marcos branco mello <k.brancomello@gmail.com>
2026-08-07 23:35:24 -0300

    Adicionar estrutura inicial da interface
    - Foi adicionado um Label
    - Foi adicionado um Frame

--- interface.py
+++ interface.py
@@ -8,4 +8,10 @@ def iniciar_interface():
 
     janela.geometry("900x600")
 
+    frame_hqs = tk.Frame(janela, bg='grey')
+    frame_hqs.pack(fill="both", expand=True)
+
+    label = tk.Label(frame_hqs,text="Minhas HQS")
+    label.pack()
+
     janela.mainloop()
